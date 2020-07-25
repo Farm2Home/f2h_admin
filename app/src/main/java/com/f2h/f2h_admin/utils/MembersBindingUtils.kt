992dcad16e9c06c3b51aa0fc53393d288151985e
Rahul Roy <rahulroy1996@gmail.com>
2020-07-25 09:32:57 +0530

    membership request bug - trim role before comparing Accept membership button to replace the delete button

--- app/src/main/java/com/f2h/f2h_admin/utils/MembersBindingUtils.kt
+++ app/src/main/java/com/f2h/f2h_admin/utils/MembersBindingUtils.kt
@@ -3,7 +3,9 @@ package com.f2h.f2h_admin.utils
 import android.view.View
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.f2h.f2h_admin.R
 import com.f2h.f2h_admin.screens.group.members.MembersUiModel
 
 
@@ -15,9 +17,12 @@ fun TextView.setRolesFormatted(data: MembersUiModel){
 @BindingAdapter("isAcceptButtonEnabled")
 fun ImageButton.setIsAcceptButtonEnabled(data: MembersUiModel){
     if (data.isBuyerRequested){
+        backgroundTintList = ContextCompat.getColorStateList(context, R.color.green_status)
         visibility = View.VISIBLE
     } else {
-        visibility = View.GONE
+        backgroundTintList = ContextCompat.getColorStateList(context, R.color.grey)
+//        setBackgroundColor(ContextCompat.getColor(context, R.color.grey))
+        visibility = View.VISIBLE
     }
 }
 
