e48e59100555ca0d00049c7a530629627c11d846
abbypjoby <abbypjoby@gmail.com>
2020-08-04 20:34:36 +0530

    image bug fix

--- app/src/main/java/com/f2h/f2h_admin/screens/group/edit_item/EditItemViewModel.kt
+++ app/src/main/java/com/f2h/f2h_admin/screens/group/edit_item/EditItemViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.MutableLiveData
 import com.cloudinary.android.MediaManager
 import com.cloudinary.android.callback.ErrorInfo
 import com.cloudinary.android.callback.UploadCallback
+import com.f2h.f2h_admin.BuildConfig
 import com.f2h.f2h_admin.database.SessionDatabaseDao
 import com.f2h.f2h_admin.database.SessionEntity
 import com.f2h.f2h_admin.network.GroupMembershipApi
@@ -203,9 +204,9 @@ class EditItemViewModel(val database: SessionDatabaseDao, application: Applicati
         _isProgressBarActive.value = true
         val requestId = MediaManager.get().upload(imageFilePath.value)
             .unsigned("unsigned_upload_settings")
-            .option("public_id", String.format("%s__%s", selectedItem.itemName, Calendar.getInstance().time))
-            .option("folder", String.format("item_images/group_%s", _sessionData.value?.groupId))
-            .option("tags", String.format("group_%s,item_%s,uploaderUserId_%s", _sessionData.value?.groupName, selectedItem.itemName,_sessionData.value?.userId))
+            .option("public_id", String.format("itemId_%s__%s", selectedItemId, Calendar.getInstance().time))
+            .option("folder", String.format("%s/group_%s/item_images", BuildConfig.ENVIRONMENT, _sessionData.value?.groupId))
+            .option("tags", String.format("group_%s,item_%s,uploaderUserId_%s", _sessionData.value?.groupId, selectedItem.itemId,_sessionData.value?.userId))
             .callback(object : UploadCallback {
                 override fun onSuccess(requestId: String, resultData: Map<*, *>?) {
                     println(resultData)
