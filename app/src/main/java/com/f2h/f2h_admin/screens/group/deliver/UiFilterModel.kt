dbd87701f9fd41cca37a0a4361bd0ee09187dbf4
Rahul Roy <rahulroy1996@gmail.com>
2020-09-30 17:40:00 +0530

    delivery agent moved to admin

---
+++ app/src/main/java/com/f2h/f2h_admin/screens/group/deliver/UiFilterModel.kt
@@ -0,0 +1,12 @@
+package com.f2h.f2h_admin.screens.group.deliver
+
+data class UiFilterModel (
+//    var selectedStartDate: String = "",
+//    var selectedEndDate: String = "",
+//    var selectedDeliveryArea: String = "",
+//    var selectedStatus: String = "",
+    var timeFilterList: List<String> = arrayListOf(),
+    var deliveryAreaList: List<String> = arrayListOf(),
+    var statusList: List<String> = arrayListOf()
+
+)
